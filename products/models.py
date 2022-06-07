79ab16809e4d29b6ec0e7696fa792ed2cfb6316f
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-06-06 22:26:24 -0500

    Updated Projeect Urls + Created First View Function

--- products/models.py
+++ products/models.py
@@ -8,3 +8,4 @@ class Product(models.Model):
      description = models.CharField(max_length=255)
      price = models.DecimalField(max_digits=8, decimal_places=2)
      inventory_quantity = models.IntegerField()
+     web_pic = models.URLField(max_length=200)
