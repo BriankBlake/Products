83960652b5a8e8c95793c5ffea3409c6debf909a
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-06-09 20:18:15 -0500

    Updated and created serializers + products_project file

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,7 @@
-from itertools import product
 from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        Model = Product
+        model = Product
         fields = ['id', 'title', 'description', 'price', 'inventory_quantity']
